8821d3e18e88bd37991c8eba9418553344f0d3d9
Daniel Komesu <danielkomesu@gmail.com>
2021-07-20 21:10:54 -0300

    Working...

--- rtnpy/table.py
+++ rtnpy/table.py
@@ -1,7 +1,8 @@
 
 from typing import Any, Callable
 
-Table = list[list[Any]]
+Column = list[Any]
+Table = list[Column]
 
 
 class Tbl:
@@ -31,6 +32,15 @@ class Tbl:
         pass
 
 
+def get_header(data: Table):
+    return list(next(zip(*data)))
+
+
+def iter_rows(data: Table):
+    for row in zip(*data):
+        yield list(row)
+
+
 def transpose(data: Table) -> Table:
     return list(zip(*data))
 
@@ -39,6 +49,7 @@ def melt(data: Table,
          id_cols: list[str],
          var_name="variable",
          value_name="value") -> Table:
+    data = transpose(data.copy())
     columns = data[0]
     index_id_cols = [columns.index(id_col) for id_col in id_cols]
     header = [*id_cols, var_name, value_name]
@@ -50,45 +61,41 @@ def melt(data: Table,
                 continue
             output_row = id_values + [columns[i], value]
             new_data.append(output_row)
+    new_data = transpose(new_data)
     return new_data
 
 
 def apply(data: Table, column_name: str, func: Callable) -> Table:
-    new_data = [data[0]]
-    index = data[0].index(column_name)
-    for row in data[1:]:
-        row[index] = func(row[index])
-        new_data.append(row)
+    header = get_header(data)
+    index = header.index(column_name)
+    new_data = data.copy()
+    new_data[index] = [new_data[index][0]] + [func(value) for value in new_data[index][1:]]
     return new_data
 
 
 def insert(data1: Table, data2: Table, index: int = 0) -> Table:
-    new_data = [data1[:index] + data2 + data1[index:]]
-    for row1, row2 in zip(data1[1:], data2[1:]):
-        new_data.append(row1[:index] + row2 + row1[index:])
+    new_data = data1[:index] + data2 + data1[index:]
     return new_data
 
 
 def select(data: Table, *columns: str) -> Table:
-    header = data[0]
+    header = get_header(data)
     indices = [header.index(column) for column in columns]
-    new_data = [header[i] for i in indices]
-    for row in data[1:]:
-        new_data.append([row[i] for i in indices])
+    new_data = [data[i] for i in indices]
     return new_data
 
 
 def where(func: Callable, data: Table) -> Table:
-    new_data = [data[0]]
-    for row in data[1:]:
-        if func(row):
-            new_data.append(row)
-    return new_data
+    pass
 
 
-def assign(data: Table, index: int, values: list[Any]) -> Table:
-    new_data = []
-    for row, new_value in zip(data, values):
-        row[index] = new_value
-        new_data.append(row)
+def assign(data: Table, column: Column) -> Table:
+    header = get_header(data)
+    new_data = data.copy()
+    column_name = column[0]
+    if column_name in header:
+        index = header.index(column_name)
+        new_data[index] = column
+    else:
+        new_data.insert(len(data), column)
     return new_data
